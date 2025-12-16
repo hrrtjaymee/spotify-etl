9b1d8739764cd2b6b6796ba21f6a012e73505ca0
hrrtjaymee <harrietregencia@gmail.com>
2025-12-16 22:34:52 +0800

    Add .gitignore, pyvenv configuration, and initial Spotify client implementation

---
+++ src/extract/spotify_client.py
@@ -0,0 +1,97 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+import os
+import json
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+load_dotenv()
+
+auth_manager = SpotifyClientCredentials(
+    client_id=os.getenv('CLIENT_ID'), 
+    client_secret=os.getenv('CLIENT_SECRET'))
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+
+def get_artists_deets(artists_lists): #without top tracks
+    artists_df = {
+        'id': [], 
+        'artist_name': [], 
+        'followers': [], 
+        'genres': [], 
+        'uri': [], 
+        'last_updated': []
+    }
+
+    for item in artists_lists:
+            
+            artist_id = item['id']
+
+            print('searching for artists')
+            current = datetime.now()
+            search_result = sp.artist(artist_id=artist_id)
+
+            #TODO: Clean inputs: id is not a number, get followers['total'], genres is empty, doublecheck uri
+            artists_df['id'].append(id)
+            artists_df['artist_name'].append(item['name'])
+            artists_df['followers'].append(search_result['followers'])
+            artists_df['genres'].append(search_result['genres'])
+            artists_df['uri'].append(search_result['uri'])
+            artists_df['last_updated'].append(current)
+    
+    return artists_df
+
+def initialize_artists(): #main function that will call other sub functions
+    playlist_id = '0fACn1Axw1sBcozazZFbsJ'
+
+    result_playlist = sp.playlist_items(playlist_id=playlist_id)
+    
+    if result_playlist is None:
+        print('No playlist found')
+        return
+    
+    tracks_items = result_playlist['items']
+    artists = [] #used as input for get_artists_deets()
+
+    while result_playlist['next']:
+        for item in tracks_items:
+            artists.extend(item['track']['artists'])
+
+        result_playlist = sp.next(result_playlist)
+
+    print(json.dumps(artists[0], indent = 4))
+
+    #calling get_artists_deets
+    artist_df = pd.DataFrame(data=get_artists_deets(artists))
+    print(artist_df[0:2])
+
+    #SAMPLE OUTPUT
+                        #    id   artist_name                           followers genres                                    uri               last_updated
+    # 0  <built-in function id>  Taylor Swift  {'href': None, 'total': 148041315}     []  spotify:artist:06HL4z0CvFAxyc27GXpf02 2025-12-16 22:19:04.853791
+    # 1  <built-in function id>   Post Malone   {'href': None, 'total': 47956715}     []  spotify:artist:246dkjvS1zLTtiykXe5h60 2025-12-16 22:19:04.928203
+
+
+initialize_artists()
+
+
+
+
+
+
+
+
+# results_2 = sp.search(q='remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis', type='artist', limit=2)
+# artists = results_2['artists']['items']
+# while results_2['artists']['next']:
+#     results_2 = sp.next(results_2['artists'])
+#     artists.extend(results_2['artists']['items'])
+
+# for artist in artists:
+#     print(artist['name'])
+
+# genre_results = sp.categories()
+# print(genre_results)
+
+
