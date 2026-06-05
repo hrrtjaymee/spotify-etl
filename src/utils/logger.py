b9f4575c798b48b6a7791793fe4ca63742b0cc82
hrrtjaymee <harrietregencia@gmail.com>
2026-06-05 12:03:24 +0800

    feat: add logger

---
+++ src/utils/logger.py
@@ -0,0 +1,21 @@
+import logging
+from datetime import datetime
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    
+    #terminal logger
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    console_handler.setFormatter(formatter)
+
+    if not logger.handlers:
+        logger.addHandler(console_handler)
+
+    return logger
